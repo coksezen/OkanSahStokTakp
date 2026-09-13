8e16c8394df5539beb10c13fba39d6d077005fdc
coksezen <coksezen764@gmail.com>
2026-09-13 20:43:28 +0300

    Load needs list alignment fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './needs-fix.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
